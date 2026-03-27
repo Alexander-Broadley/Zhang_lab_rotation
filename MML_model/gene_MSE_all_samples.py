40ea18b963d424cd309c4344f1105a840b0591af
Alexander-Broadley <zcbtroa@ucl.ac.uk>
2026-03-27 12:03:44 +0000

    Re-arranged pipeline into scripts. Moved other work for project into directory.

--- MML_model/gene_MSE_all_samples.py
+++ MML_model/gene_MSE_all_samples.py
@@ -1,7 +1,9 @@
 import torch
+import math
+import numpy as np
 
 #define a function to calculate the MSE 
-def gene_MSE_all_samples(dataloader, model, loss_fn, target_gene):
+def gene_MSE_all_samples(dataloader, model, loss_fn, target_gene, rev_log = False):
     #put model in eval mode
     model.eval()
     with torch.no_grad():
@@ -9,6 +11,11 @@ def gene_MSE_all_samples(dataloader, model, loss_fn, target_gene):
 
             #create a prediction - this will be 1 value for given sample and target gene
             pred = model(X)
+
+            #for log(TPM+1) model reverse the transforms so MSE is comparable with TPM models
+            if rev_log == True:
+                pred = pred.expm1()
+                y = y.expm1()
             
             #calculate prediction loss - this will be one loss value in a distribution across samples per target gene
             loss = loss_fn(pred, y)
