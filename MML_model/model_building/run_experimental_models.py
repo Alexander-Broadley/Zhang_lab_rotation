23a7db34c2d43871a52d10c06724a96d736e0a6c
Alexander-Broadley <zcbtroa@ucl.ac.uk>
2026-05-05 10:15:08 +0000

    First try adding .gitkeep to preserve directory structure

--- MML_model/model_building/run_experimental_models.py
+++ MML_model/model_building/run_experimental_models.py
@@ -23,11 +23,12 @@ from model_building.early_stopper import EarlyStopping
 from model_building.activation_functions import activation_function_map
 #import model
 from model_building.simpleMMLModel import SimpleMMLModel
-#import MSE per batch calculator
-from Zhang_lab.MML_model.model_building.batch_loss import gene_MSE_all_samples
+#import function to calculate loss per batch (in this case 1 batch is all samples)
+from model_building.batch_loss import batch_loss
 #import function to train a single epoch
 from model_building.train_one_epoch import train_one_epoch
 
+from model_building.filter_dataset import filter_datasets
 #import dataset object
 from model_building.customTFGE_dataset import CustomTFGE
 
@@ -35,35 +36,24 @@ device = torch.accelerator.current_accelerator().type if torch.accelerator.is_av
 print(f"Using {device} device")
 
 #define root directory
-DATA_ROOT = '../data'
-
-#get low scoring models to assess improvements
-current_results = pd.read_csv(f'{DATA_ROOT}/PEARSONS_results_ACC.csv', index_col = 0, header = 0)
-
-low_scoring_models = list(current_results[current_results['train_loss'] > 1].index)
+DATA_ROOT = '../../data'
 
 print('Loading Datasets')
 #Load network
 net = pd.read_csv(f"{DATA_ROOT}/Full data files/network(full).tsv", sep='\t')
 #Load target gene expressions
-#gene_expressions = pd.read_csv(f"{DATA_ROOT}/Full data files/Genexpression_TPM.tsv", sep='\t', index_col=0, header=0)
-#TF_expressions = pd.read_csv(f"{DATA_ROOT}/Full data files/TF_TPM.tsv", sep='\t', index_col=0, header=0)
 
+#try new filtering function
+gene_expressions = pd.read_csv((f"{DATA_ROOT}/Full data files/ARCHS4_healthy_log.tsv"), sep='\t', header=0)
+print(f'Before subsetting to external GE has shape {gene_expressions.shape}')
 
-gene_expressions = pd.read_csv((f"{DATA_ROOT}/Full data files/Geneexpression (full).tsv"), sep='\t', header=0)
-TF_expressions = pd.read_csv((f"{DATA_ROOT}/Full data files/TF(full).tsv"), sep='\t', header=0)
+#to get validation on external dataset to be more fair filter to only genes in the external dataset
+external_expressions = pd.read_csv(f'{DATA_ROOT}/Full data files/Liver_bulk_external.tsv', index_col = 0, sep = '\t')
 
-#filter network to only include TFs that are in the dataset
-net = net[net['TF'].isin(TF_expressions.columns)]
+gene_expressions = gene_expressions[[gene for gene in gene_expressions.columns if gene in external_expressions.columns]]
+print(f'After subsetting to external GE has shape {gene_expressions.shape}')
 
-print('Filtering genes in datasets')
-#filter genes to nodes in network
-network_tfs = set(net['TF'].unique())      # TFs
-network_genes = set(net['Gene'].unique())  # target genes
-#network_nodes = network_tfs | network_genes
-
-TF_expressions = TF_expressions[[gene for gene in TF_expressions.columns if gene in list(network_tfs)]]
-gene_expressions = gene_expressions[[gene for gene in gene_expressions.columns if gene in list(network_genes)]] 
+TF_expressions, gene_expressions = filter_datasets(net, GE_df=gene_expressions)
 
 #define function to subset transcription factors to only those that directly regulate the target gene
 def TF_subset(net, target_gene):
@@ -71,113 +61,104 @@ def TF_subset(net, target_gene):
     #returns all the TFs in the network that directly connect to the target gene
     return(list(net['TF'][net['Gene'] == target_gene]))
 
-
 #define training parameters
 learning_rate = 1e-3
 #run 1 sample at a time, but run through each sample per training epoch
 batch_size = TF_expressions.shape[0]
 #max 100 epochs
-epochs =  100 #12747 * 5
-#initialize MSE loss function - same as LEMBAS
-#loss_fn = nn.MSELoss()
+epochs =  200 #12747 * 5
 
 #trying with new loss_fn
-from model_building.stable_pearsons_loss import PearsonLoss
+from model_building.pearsons_loss import PearsonLoss
 loss_fn = PearsonLoss()
 
 #create a results dataframe
-results_df = pd.DataFrame(columns = ['train_loss', 'test_loss', 'stopped_early', 'in_features'])
+results_df = pd.DataFrame(index = gene_expressions.columns, columns = ['train_loss', 'test_loss', 'stopped_early', 'stopped_epoch','in_features'])
 
 #create dataframes to track the predicted and actual expressions for train and test datasets - recreating datasets with pytorch is unreliable and cannot store 161000 datasets
-
-
 #intialise training dataset predicted values df
-train_predicted = pd.DataFrame(columns = gene_expressions.columns)
-test_predicted = pd.DataFrame(columns = gene_expressions.columns)
+train_predicted = pd.DataFrame(columns=gene_expressions.columns)
+test_predicted = pd.DataFrame(columns=gene_expressions.columns)
 
 #initialise training dataset actual values df - easier to do it this way as can get values after torch train-test split
-train_actual = pd.DataFrame(columns = gene_expressions.columns)
-test_actual = pd.DataFrame(columns = gene_expressions.columns)
+train_actual = pd.DataFrame(columns=gene_expressions.columns)
+test_actual = pd.DataFrame(columns=gene_expressions.columns)
 
-failed_models = 0
+print('TF expressions shape:', TF_expressions.shape)
+print('Target gene expressions shape:', gene_expressions.shape)
 #for the remaining code need to execute per target gene (per gene in gene_expressions)
-for target_gene in gene_expressions[low_scoring_models]:
-    print(target_gene)
+for target_gene in gene_expressions.columns:
+    print(f'Creating model for {target_gene}')
+    
+    #make TF_expression_subset an instance of TF_expressions to handle dropping a TF non-permanently if necessary
+    TF_expression_subset = TF_expressions
+    if target_gene in TF_expressions.columns:
+        print('Target gene is a TF, removing from TF dataset')
+        TF_expression_subset = TF_expressions.drop(target_gene, axis = 1)
 
-    #this catches errors due to instability in the pearsons correlation function that have arisen after dataset transformation
-    try:
-        #initialise an early stopper to end training if loss on test data does not fall by at least 0.01 MSE for 3 eopochs in a row
-        early_stopping = EarlyStopping(patience=3, delta=0.01, verbose=True)
-        
-        #print(f'Creating model for {target_gene}')
-        TF_expression_subset = TF_expressions[TF_subset(net, target_gene)]
+    #initialise an early stopper to end training if loss on test data does not fall by at least 0.01 MSE for 3 eopochs in a row
+    early_stopping = EarlyStopping(patience=3, delta=0.005, verbose=True)
+    
+    #TF_expression_subset = TF_expressions#[TF_subset(net, target_gene)]
 
-        dataset = CustomTFGE(device, TF_expressions=TF_expression_subset, gene_expressions=gene_expressions, network = net, target_gene = target_gene)
+    dataset = CustomTFGE(device, TF_expressions=TF_expression_subset, gene_expressions=gene_expressions, network = net, target_gene = target_gene)
+    train_dataset, test_dataset = torch.utils.data.random_split(dataset, [0.8, 0.2], generator=torch.Generator().manual_seed(42))
 
-        train_dataset, test_dataset = torch.utils.data.random_split(dataset, [0.8, 0.2], generator=torch.Generator().manual_seed(42))
+    model = SimpleMMLModel(activation_function_map['MML'], TF_expression_subset.shape[1])
 
-        model = SimpleMMLModel(activation_function_map['MML'], TF_expression_subset.shape[1])
+    if torch.cuda.device_count() > 1:
+        model = nn.DataParallel(model)
 
-        if torch.cuda.device_count() > 1:
-            model = nn.DataParallel(model)
+    model.to(device)
 
-        model.to(device)
+    #initialise same optimiser as LEMBAS
+    optimiser = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay = 0.00001)
 
-        #initialise same optimiser as LEMBAS
-        optimiser = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False)
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
-        train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False)
-        test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    for t in range(epochs):
+        train_loss = train_one_epoch(train_dataloader, model, loss_fn, optimiser)
+        test_loss = batch_loss(test_dataloader, model, loss_fn)
 
-        for t in range(epochs):
-            
-            train_loss = train_one_epoch(train_dataloader, model, loss_fn, optimiser)
+        if (t+1) % 5 == 0:
+            print(f"Epoch {t+1}\n-------------------------------")
+            print(test_loss)
 
-            test_loss = gene_MSE_all_samples(test_dataloader, model, loss_fn, target_gene)
-            #print(f'Test MSE this epoch is: {test_loss}')
+        early_stopping.check_early_stop(test_loss)
+    
+        if early_stopping.stop_training:
+            print(f"Early stopping at epoch {t+1}")  
+            results_df.loc[target_gene, 'stopped_early'] = 1
+            results_df.loc[target_gene, 'stopped_epoch'] = t+1
+            break
 
-            if (t+1) % 5 == 0:
-                print(f"Epoch {t+1}\n-------------------------------")
-                print(test_loss)
+    #put model in eval mode
+    model.eval()
+    with torch.no_grad():
+        #for each dataset then load the data (using same seed as training to get same train test split)
+        #get the predicted and actual score this way as easiest way to get values from the torch train test split
+        for batch, (X, y) in enumerate(train_dataloader):
+            train_predicted[target_gene] = model(X).cpu()
+            train_actual[target_gene] = y.cpu()
 
-            early_stopping.check_early_stop(test_loss)
-        
-            if early_stopping.stop_training:
-                print(f"Early stopping at epoch {t+1}")  
-                results_df.loc[target_gene, 'stopped_early'] = 1
-                break
+        for batch, (X, y) in enumerate(test_dataloader):
+            test_predicted[target_gene] = model(X).cpu()
+            test_actual[target_gene] = y.cpu()
 
-        #put model in eval mode
-        model.eval()
-        with torch.no_grad():
-            #for each dataset then load the data (using same seed as training to get same train test split)
-            #get the predicted and actual score this way as easiest way to get values from the torch train test split
-            for batch, (X, y) in enumerate(train_dataloader):
-                train_predicted[target_gene] = model(X).cpu()
-                train_actual[target_gene] = y.cpu()
-
-            for batch, (X, y) in enumerate(test_dataloader):
-                test_predicted[target_gene] = model(X).cpu()
-                test_actual[target_gene] = y.cpu()
-
-            results_df.loc[target_gene, 'train_loss'] = train_loss
-            results_df.loc[target_gene, 'test_loss'] = test_loss
-            results_df.loc[target_gene, 'in_features'] = len(TF_expression_subset.columns)
-            torch.save(model, f'../models/experimental_models/{target_gene}_model.pth')
-    except:
-        print(f'{target_gene} model failed')
-        failed_models += 1
-
-print(f'{failed_models} models failed')
+    results_df.loc[target_gene, 'train_loss'] = train_loss
+    results_df.loc[target_gene, 'test_loss'] = test_loss
+    results_df.loc[target_gene, 'in_features'] = len(TF_expression_subset.columns)
+    torch.save(model, f'../models/experimental_models/{target_gene}_model.pth')
 
 
-train_actual.to_csv(f'./data/Train_dataset_actual_expressions_EXP.csv')
-train_predicted.to_csv(f'./data/Train_dataset_predicted_expressions_EXP.csv')
+train_actual.to_csv(f'{DATA_ROOT}/Train_dataset_actual_expressions_EXPERIMENTAL.csv')
+train_predicted.to_csv(f'{DATA_ROOT}/Train_dataset_predicted_expressions_EXPERIMENTAL.csv')
 
-test_actual.to_csv(f'./data/Test_dataset_actual_expressions_EXP.csv')
-test_predicted.to_csv(f'./data/Test_dataset_predicted_expressions_EXP.csv')
+test_actual.to_csv(f'{DATA_ROOT}/Test_dataset_actual_expressions_EXPERIMENTAL.csv')
+test_predicted.to_csv(f'{DATA_ROOT}/Test_dataset_predicted_expressions_EXPERIMENTAL.csv')
 
 
-results_df.to_csv('./data/EXPERIMENTAL_results.csv')
+results_df.to_csv('../../data/2500_samples_results.csv')
 
-print('Training of experimental models: 200 epochs, healthy ARCHS4, randn bias: finished')
+print('Finished Healthy models all TFs')
