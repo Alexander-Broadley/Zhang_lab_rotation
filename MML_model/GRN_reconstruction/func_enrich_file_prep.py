54eb28e905f9e74add18177e84666afc98ca8726
Alexander-Broadley <zcbtroa@ucl.ac.uk>
2026-05-18 12:46:10 +0100

    functional enrichment scripts

--- MML_model/GRN_reconstruction/func_enrich_file_prep.py
+++ MML_model/GRN_reconstruction/func_enrich_file_prep.py
@@ -15,7 +15,6 @@ net = pd.read_csv(f"{DATA_ROOT}/Full data files/network(full).tsv", sep='\t')
 
 #try new filtering function
 gene_expressions = pd.read_csv((f"{DATA_ROOT}/Full data files/ARCHS4_healthy_log_noFMExternal.tsv"), sep='\t', header=0, index_col=0)
-print(gene_expressions.head)
 
 TF_expressions, gene_expressions = filter_datasets(net, GE_df=gene_expressions)
 
@@ -25,6 +24,8 @@ background_TFs = set(TF_expressions.columns)
 
 background_genes = background_TFs | background_TGs
 
-print(background_genes)
-print(len(background_genes), len(background_TFs), len(background_TGs))
 
+with open('./background.txt', 'w+') as f:
+    for gene in background_genes:
+        f.write(f'{gene}\n')
+f.close
